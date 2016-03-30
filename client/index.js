99674e4972d2c4903fa357266c09763b60d74488
evenfrost <kisolov@gmail.com>
2016-03-31 00:15:26 +0300

    Update

--- client/index.js
+++ client/index.js
@@ -2,14 +2,25 @@ import bulker from 'bulker';
 
 const create = document.createElement.bind(document);
 const list = document.querySelector('ul');
+const items = bulker('li');
 
 const observer = new MutationObserver(mutations => {
-  mutations.forEach(mutation => console.log(mutation));
+  mutations.forEach(mutation => {
+    Array.from(mutation.addedNodes)
+      .forEach(node => {
+        console.log(node);
+        console.log(node.matches && node.matches(items._selectors[0]));
+      });
+  });
 });
 
-observer.observe(document.body);
+console.log('items', items);
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
+});
 
 list.appendChild(create('li'));
 list.appendChild(create('li'));
 list.appendChild(create('li'));
-
